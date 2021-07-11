ec1bb0230e2408b7c0eb6fbaf4b39303d418f32e
root <bipul8765@gmail.com>
2021-07-11 17:51:51 +0530

    Forth Commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app=express();
 //app.use(bodyParser.urlencoded({extended : true}));
 
 
-mongoose.connect("mongodb://localhost:27017/Internship",{bufferMaxEntries: 0, reconnectTries: 5000, useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://Bipul70701:@Bipul123@cluster0.zbvcq.mongodb.net/Internship",{bufferMaxEntries: 0, reconnectTries: 5000, useNewUrlParser: true,useUnifiedTopology: true});
 
 app.use(express.static('public'));
 app.set("view engine",'ejs');
@@ -181,7 +181,7 @@ app.get("/",function(req,res)
 {
    res.sendFile(__dirname+"/login.html");
 });
-app.listen(3000,function ()
+app.listen(process.env.PORT || 3000,function ()
 {
     console.log("Server started on port 3000");
 });
